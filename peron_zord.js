0d8c77965efc82929f62d65f76f3ae7ba773acd4
Pablo Martin Lehoczky <pmlehoczky@gmail.com>
2020-05-24 23:16:00 -0300

    agregada lógica rudimentaria de disparos

--- peron_zord.js
+++ peron_zord.js
@@ -1,21 +1,34 @@
+let y;
+let shots = [];
+let shotSpeed = 5;
+
 
 function setup() {
-  createCanvas(400, 400);
-  frameRate(20);
+  createCanvas(300, 500);
+  frameRate(50);
+  y = height * 2 / 3;
 }
 
 function draw() {
   background(220);
 
   
-  s1 = new Ship(12,14, height, width);
+  s1 = new Ship(12, 14, height, width);
   s1.setX(mouseX);
   s1.drawShip();
-  
 
+  
+  //s1.shoot();
+  
   text(key, 33, 65); // Display last key pressed.
-  
 
-  
+  shots.forEach(element => {
+    element.moveForward();
+  });
+
 } 
+
+function mouseClicked() {
+  shots.push(s1.shoot(shotSpeed));
+};
 
--- ship.js
+++ ship.js
@@ -1,64 +1,52 @@
-/* var baseLength;
-var shipLength;
-var Yposition;
-var Xposition;
-var vertexPositionLD;
-var vertexPositionLR;
-var vertexPositionU;
-
-baseLength = 12;
-shipLength = 14;
-Yposition = height * 4 / 5;
-Xposition = width/2 - baseLength/2;
-vertexPositionLD = createVector(Xposition, Yposition);
-vertexPositionLR = createVector(Xposition + baseLength, Yposition);
-vertexPositionU = createVector(Xposition + baseLength / 2, Yposition - shipLength);
-
-function drawShip() {
-    triangle(vertexPositionLD.x, vertexPositionLD.y,
-        vertexPositionLR.x, vertexPositionLR.y,
-        vertexPositionU.x, vertexPositionU.y);
- 
-}  */
 
 function Ship(baseLength, shipLength, height, width) {
     this.baseLength = baseLength;
     this.shipLength = shipLength;
     this.Yposition = height * 4 / 5;
     this.Xposition = width/2 - baseLength/2; 
-    //this.vertexPositionLD = createVector(this.Xposition, this.Yposition);
     this.vertexPositionLD = [this.Xposition, this.Yposition];
-    //this.vertexPositionLR = createVector(this.Xposition + baseLength, this.Yposition);
     this.vertexPositionLR = [this.Xposition + baseLength, this.Yposition];
-    //this.vertexPositionU = createVector(this.Xposition + baseLength / 2, this.Yposition - shipLength);
     this.vertexPositionU = [this.Xposition + baseLength / 2, this.Yposition - shipLength];
     
     this.drawShip = function() {
-        // triangle(this.vertexPositionLD.x, this.vertexPositionLD.y,
-        //     this.vertexPositionLR.x, this.vertexPositionLR.y,
-        //     this.vertexPositionU.x, this.vertexPositionU.y);
+
         triangle(this.vertexPositionLD[0], this.vertexPositionLD[1],
             this.vertexPositionLR[0], this.vertexPositionLR[1],
-                this.vertexPositionU[0], this.vertexPositionU[1]);
+            this.vertexPositionU[0], this.vertexPositionU[1]);
     
     };
 
     this.setX = function(x) {
         this.Xposition = x;
         this.vertexPositionLD = [this.Xposition, this.Yposition];
-        //this.vertexPositionLR = createVector(this.Xposition + baseLength, this.Yposition);
         this.vertexPositionLR = [this.Xposition + baseLength, this.Yposition];
-        //this.vertexPositionU = createVector(this.Xposition + baseLength / 2, this.Yposition - shipLength);
         this.vertexPositionU = [this.Xposition + baseLength / 2, this.Yposition - shipLength];
-        /* triangle(this.vertexPositionLD[0], this.vertexPositionLD[1],
-                this.vertexPositionLR[0], this.vertexPositionLR[1],
-                    this.vertexPositionU[0], this.vertexPositionU[1]); */
 
     };
 
     this.setY = function(y) {
         this.Yposition = y;
+    };
+
+    this.shoot = function(speed) {
+        return new Shot(this.Xposition + baseLength / 2, this.Yposition - shipLength, speed);
 
     };
 
 }
+
+function Shot(x, y, speed) {
+    this.x = x;
+    this.y = y;
+        strokeWeight(8);
+        point(x, this.y);
+        strokeWeight(1);
+    this.moveForward = function() {
+        this.y = this.y - speed;
+        strokeWeight(8);
+        point(x, this.y);
+        strokeWeight(1);
+    };
+}
+
+        
