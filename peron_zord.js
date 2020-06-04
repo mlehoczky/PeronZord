0a4b4c8122f89ee5b261d89bf7105d4c676bf571
Pablo Martin Lehoczky <pmlehoczky@gmail.com>
2020-06-04 00:19:29 -0300

    Agregado Sonido.

--- peron_zord.js
+++ peron_zord.js
@@ -1,10 +1,18 @@
 let y;
 let shots = [];
 let shotSpeed = 5;
+let lastTimeShot = 0;
+let shotDelay = 200;
+//let shotFrequency = 1.05;
+
+function preload() {
+  soundFormats('ogg');
+  pewSound = loadSound('assets/sound/pew');
+}
 
 
 function setup() {
-  //createCanvas(300, 500);
+  // createCanvas(300, 500);
   createCanvas(windowWidth, windowHeight);
   frameRate(50);
   y = height * 2 / 3;
@@ -22,14 +30,19 @@ function draw() {
   //s1.shoot();
   
   text(key, 33, 65); // Display last key pressed.
+  //text("lastTimeShot: " + lastTimeShot + "millis: " + millis(), 33, 65);
+  let now = millis();
 
-  if (mouseIsPressed) {
-    if (millis() % 10 == 0) shots.push(s1.shoot(shotSpeed));
+  //if (mouseIsPressed && (millis() > lastTimeShot ** shotFrequency)) {
+  if (mouseIsPressed && ((now - lastTimeShot) > shotDelay) ) {
+    shots.push(s1.shoot(shotSpeed));
+    pewSound.play();
+    lastTimeShot = millis();
   };
 
-  if (touches.length > 0) {
-    if (millis() % 10 == 0) shots.push(s1.shoot(shotSpeed));
-  };
+  // if (touches.length > 0) {
+  //   if (millis() > lastTimeShot ** shotFrequency) shots.push(s1.shoot(shotSpeed));
+  // };
 
 
   shots.forEach(element => {
